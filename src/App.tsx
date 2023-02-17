243b7d61b520052c48de09019face3dd7240be72
dzmitriy78 <93944218+dzmitriy78@users.noreply.github.com>
2023-02-17 14:17:29 +0300

    add: skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 import Sort from "./components/Sort/Sort";
 import ItemBlock, {ItemType} from "./components/ItemBlock/ItemBlock";
+import axios from "axios";
+import Skeleton from "./components/ItemBlock/Skeleton";
 
 const App: React.FC = () => {
 
     const [items, setItems] = React.useState<ItemType[]>([])
+    const [isFetching, setIsFetching] = React.useState(false)
 
     React.useEffect(() => {
-        fetch("https://63ea74ede0ac9368d6525c20.mockapi.io/shop-items")
-            .then(value => value.json())
-            .then(res => setItems(res))
+        setIsFetching(true)
+        axios.get<ItemType[]>("https://63ea74ede0ac9368d6525c20.mockapi.io/shop-items")
+            .then(res => res.data)
+            .then(data => {
+                setItems(data)
+                setIsFetching(false)
+            })
     }, [])
     return (
         <div className="App">
@@ -25,11 +32,14 @@ const App: React.FC = () => {
                             <Sort/>
                         </div>
                         <h2 className="content__title">Все чайники</h2>
-                        <div className="content__items">
-                            {items.map((item, i) => {
-                                return <ItemBlock key={i} {...item}/>
-                            })}
-                        </div>
+                        {isFetching
+                            ? <div className="content__items">
+                                {[...new Array(9)].map((_, i) => <Skeleton key={i}/>)}
+                            </div>
+                            : <div className="content__items">
+                                {items.map((item, i) => <ItemBlock key={i} {...item}/>)}
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
---
+++ src/components/ItemBlock/Skeleton.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = () => (
+    <ContentLoader
+        className="pizza-block"
+        speed={2}
+        width={280}
+        height={500}
+        viewBox="0 0 280 500"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+    >
+        <rect x="12" y="16" rx="0" ry="0" width="1" height="0"/>
+        <rect x="20" y="0" rx="14" ry="14" width="260" height="260"/>
+        <rect x="20" y="275" rx="13" ry="13" width="260" height="47"/>
+        <rect x="20" y="334" rx="14" ry="14" width="260" height="86"/>
+        <rect x="125" y="436" rx="25" ry="25" width="156" height="44"/>
+        <rect x="20" y="438" rx="13" ry="13" width="91" height="27"/>
+    </ContentLoader>
+)
+
+export default Skeleton
